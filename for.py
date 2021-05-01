617b17e1db1d4e47c05894e8a5e7ff81042ad081
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-05-01 16:46:55 +0300

    SHORTEST WAY TO FORM STRING

---
+++ for.py
@@ -0,0 +1,21 @@
+def shortestWayToFormString(source, target):
+  num_subsequences = 0
+  remaining = target
+  
+  while len(remaining) > 0:
+    subsequence = ""
+    i = 0
+    j = 0
+    
+    while i < len(source) and j < len(remaining):
+      if source[i] == remaining[j]:
+        subsequence += remaining[j]
+        j += 1
+        
+    if len(subsequence) == 0:
+      return -1
+    else:
+      num_subsequences += 1
+      remaining = remaining[len(subsequence) : ]
+      
+  return num_subsequences
