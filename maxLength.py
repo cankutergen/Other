4d251e8a557921b604c06fbb37aece0d1a74d82d
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-05-06 14:47:09 +0300

    Maximum Length of a Concatenated String with Unique Characters

--- maxLength.py
+++ maxLength.py
@@ -1,19 +1,37 @@
+import string
+
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
         
-        def has_dublicate(s):
-            return len(s) != len(set(s))
-        
-        def dfs(current, index):
-            nonlocal result
-            result = max(result, len(current))
-            
-            for i in range(index, len(arr)):
-                new_string = current + arr[i]
-                
-                if not has_dublicate(new_string):
-                    dfs(new_string, index + 1)
-                    
         result = 0
-        dfs("", 0)
+        
+        def max_unique(index, current):
+            nonlocal result
+            if index == len(arr) and unique_char_count(current) > result:
+                result = len(current)
+                return
+            
+            if index == len(arr):
+                return
+            
+            max_unique(index + 1, current)
+            max_unique(index + 1, current + arr[index])
+                
+                
+        def unique_char_count(current):
+            counts = [0] * 26
+            
+            values = dict()
+            for index, letter in enumerate(string.ascii_lowercase):
+                values[letter] = index
+            
+            for char in current:
+                if counts[values[char]] > 0:
+                    return -1
+                
+                counts[values[char]] += 1
+            
+            return len(current)
+        
+        max_unique(0, "")
         return result
