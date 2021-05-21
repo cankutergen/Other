15cd9d77d95ab87a9babfb02ddeb0d1d18b30f10
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-05-21 15:51:08 +0300

    kadane

--- kadane.py
+++ kadane.py
@@ -2,7 +2,7 @@ def kadane(A):
   max_current = A[0]
   max_global = A[0]
   
-  for i in range(len(A)):
+  for i in range(1, len(A)):
     max_current = max(A[i], max_current + A[i])
     
     if max_current > max_global:
